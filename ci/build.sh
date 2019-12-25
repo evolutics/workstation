7ba471a2db248f42a50f76f07f31bf338787f157
Benjamin Fischer <benjamin.fischer@evolutics.info>
2019-12-25 16:56:29 +0100

    Introduce build to check code lint and format

---
+++ ci/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+main() {
+  local -r script_folder="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  local -r project_folder="$(dirname "${script_folder}")"
+
+  pushd "${project_folder}"
+
+  local -r image_id="$(docker build --quiet \
+    --build-arg ansible_lint=4.2.0 \
+    --build-arg git=2.24.1 \
+    --build-arg gitlint=0.12.0 \
+    --build-arg hunspell=1.7.0 \
+    --build-arg prettier=1.19.1 \
+    https://github.com/evolutics/code-cleaner-buffet.git#0.6.0)"
+  docker run --rm --volume "$(pwd)":/workdir "${image_id}" ci/check.sh
+
+  popd
+}
+
+main "$@"
---
+++ ci/check.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+check_with_ansible_lint() {
+  ansible-lint local.yml
+}
+
+check_with_git() {
+  git diff --check HEAD^
+}
+
+check_with_gitlint() {
+  gitlint --config ci/.gitlint
+}
+
+check_with_hunspell() {
+  git log --format=%B | hunspell -l -d en_US -p ci/personal_words.dic \
+    | sort | uniq | tr '\n' '\0' | xargs -0 -r -n 1 sh -c \
+    'echo "Misspelling: $@"; exit 1' --
+}
+
+check_with_prettier() {
+  prettier --check '**/*.+(json|md|yaml|yml)'
+}
+
+main() {
+  check_with_ansible_lint
+  check_with_git
+  check_with_gitlint
+  check_with_hunspell
+  check_with_prettier
+}
+
+main "$@"
