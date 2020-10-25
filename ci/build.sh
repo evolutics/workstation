f19493aabed85a70a3334e5314195f08470954f6
Benjamin Fischer <benjamin.fischer@evolutics.info>
2020-10-25 23:05:31 +0100

    Extract code cleaner check

--- ci/build.sh
+++ ci/build.sh
@@ -10,14 +10,9 @@ main() {
 
   pushd "${project_folder}"
 
-  local -r image_id="$(docker build --quiet \
-    --build-arg ansible_lint=4.2.0 \
-    --build-arg git=2.24.3 \
-    --build-arg gitlint=0.13.1 \
-    --build-arg hunspell=1.7.0 \
-    --build-arg prettier=2.0.5 \
-    https://github.com/evolutics/code-cleaner-buffet.git#0.10.0)"
-  docker run --rm --volume "$(pwd)":/workdir "${image_id}" ci/check.sh
+  docker run --entrypoint sh --rm --volume "$(pwd)":/workdir \
+    evolutics/travel-kit:0.2.0 -c \
+    'git ls-files -z | xargs -0 travel-kit check --'
 
   popd
 }
