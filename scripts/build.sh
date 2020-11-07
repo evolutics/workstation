6c0552094eac99d56b6c8e42f74c940ee8898c58
Benjamin Fischer <benjamin.fischer@evolutics.info>
2020-11-07 18:40:32 +0100

    Update build dependencies, running Ansible Lint separately

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,8 +11,13 @@ main() {
   pushd "${project_folder}"
 
   docker run --entrypoint sh --rm --volume "$(pwd)":/workdir \
-    evolutics/travel-kit:0.3.0 -c \
+    evolutics/travel-kit:0.5.0 -c \
     'git ls-files -z | xargs -0 travel-kit check --'
+
+  docker run --rm --volume "$(pwd)":/workdir \
+    "$(DOCKER_BUILDKIT=1 docker build --build-arg ansible_lint=4.3.5 --quiet \
+    https://github.com/evolutics/code-cleaner-buffet.git#0.14.0)" \
+    ansible-lint
 
   popd
 }
