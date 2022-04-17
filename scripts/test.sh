383db791b410b7dd5c7dc50dc9d215a384c2104d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-04-17 21:53:01 +0200

    Update dependencies

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,7 +9,7 @@ main() {
   cd "$(dirname "${script_folder}")"
 
   docker run --entrypoint sh --rm --volume "${PWD}":/workdir \
-    evolutics/travel-kit:0.7.0 -c \
+    evolutics/travel-kit:0.8.0 -c \
     'git ls-files -z | xargs -0 travel-kit check --'
 
   docker run --rm --volume "${PWD}":/workdir \
