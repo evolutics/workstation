9d29d62b6569941812b909d728eaa9a1e3aebf30
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-04-17 11:56:04 +0200

    Simplify folder handling

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,14 +6,13 @@ set -o pipefail
 
 main() {
   local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
-  local -r project_folder="$(dirname "${script_folder}")"
-  cd "${project_folder}"
+  cd "$(dirname "${script_folder}")"
 
-  docker run --entrypoint sh --rm --volume "$(pwd)":/workdir \
+  docker run --entrypoint sh --rm --volume "${PWD}":/workdir \
     evolutics/travel-kit:0.7.0 -c \
     'git ls-files -z | xargs -0 travel-kit check --'
 
-  docker run --rm --volume "$(pwd)":/workdir \
+  docker run --rm --volume "${PWD}":/workdir \
     "$(DOCKER_BUILDKIT=1 docker build --build-arg ansible_lint=5.3.1 --quiet \
       https://github.com/evolutics/code-cleaner-buffet.git#0.17.0)" \
     ansible-lint
