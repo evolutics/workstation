f9f485e29aeed5cacbd6ea885cd591591e2f6a68
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-11-18 22:01:20 +0100

    Add minimal Git

--- home.nix
+++ home.nix
@@ -6,7 +6,17 @@
   home.username = "foo";
   home.homeDirectory = "/home/foo";
 
+  home.packages = [
+    pkgs.git
+  ];
+
   home.stateVersion = "22.05";
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Foo Bar";
+    userEmail = "foo@example.com";
+  };
 }
