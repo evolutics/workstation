1ab881e046495efb36b11885d95c6ddd58e94c47
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-11-19 12:33:04 +0100

    Nest as more natural

--- home.nix
+++ home.nix
@@ -3,36 +3,40 @@
   pkgs,
   ...
 }: {
-  home.username = "foo";
-  home.homeDirectory = "/home/foo";
+  home = {
+    username = "foo";
+    homeDirectory = "/home/foo";
 
-  home.packages = [
-    pkgs.ansible
-    pkgs.git
-    pkgs.pdftk
-    pkgs.vscode
-  ];
+    packages = [
+      pkgs.ansible
+      pkgs.git
+      pkgs.pdftk
+      pkgs.vscode
+    ];
 
-  home.stateVersion = "22.05";
+    stateVersion = "22.05";
+  };
 
   nixpkgs.config.allowUnfree = true;
 
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
 
-  programs.bash = {
-    enable = true;
-    historyControl = ["ignoredups" "ignorespace"];
-    historyFileSize = 200000;
-    historySize = 100000;
-    shellAliases = {
-      ls = "ls --color=auto";
+    bash = {
+      enable = true;
+      historyControl = ["ignoredups" "ignorespace"];
+      historyFileSize = 200000;
+      historySize = 100000;
+      shellAliases = {
+        ls = "ls --color=auto";
+      };
     };
-  };
 
-  programs.git = {
-    enable = true;
-    userName = "Foo Bar";
-    userEmail = "foo@example.com";
+    git = {
+      enable = true;
+      userName = "Foo Bar";
+      userEmail = "foo@example.com";
+    };
   };
 
   xfconf.settings = {
