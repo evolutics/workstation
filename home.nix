298ddea5bcd0416521cd79dab3cbae2f43adadb2
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-11-20 11:49:03 +0100

    Move import as it is implementation detail

--- home.nix
+++ home.nix
@@ -1,100 +1,99 @@
-let
+{
+  config,
+  pkgs,
+  ...
+}: let
   customization = import ./customization.nix;
-in
-  {
-    config,
-    pkgs,
-    ...
-  }: {
-    home = {
-      file.".config/nix/nix.conf".text = ''
-        experimental-features = flakes nix-command
-      '';
+in {
+  home = {
+    file.".config/nix/nix.conf".text = ''
+      experimental-features = flakes nix-command
+    '';
 
-      homeDirectory = "/home/${customization.username}";
+    homeDirectory = "/home/${customization.username}";
 
-      packages = let
-        extra_packages = customization.extra_packages pkgs;
-        standard_packages = with pkgs; [
-          alejandra
-          ansible
-          docker
-          flameshot
-          git
-          keepassxc
-          pdftk
-          podman
-          vagrant
-          vlc
-          vscode
-          vscode-extensions.eamodio.gitlens
-          vscode-extensions.esbenp.prettier-vscode
-          vscode-extensions.kamadorueda.alejandra
-          vscode-extensions.streetsidesoftware.code-spell-checker
-          vscode-extensions.timonwong.shellcheck
-          xclip
-        ];
-      in
-        standard_packages ++ extra_packages;
+    packages = let
+      extra_packages = customization.extra_packages pkgs;
+      standard_packages = with pkgs; [
+        alejandra
+        ansible
+        docker
+        flameshot
+        git
+        keepassxc
+        pdftk
+        podman
+        vagrant
+        vlc
+        vscode
+        vscode-extensions.eamodio.gitlens
+        vscode-extensions.esbenp.prettier-vscode
+        vscode-extensions.kamadorueda.alejandra
+        vscode-extensions.streetsidesoftware.code-spell-checker
+        vscode-extensions.timonwong.shellcheck
+        xclip
+      ];
+    in
+      standard_packages ++ extra_packages;
 
-      stateVersion = "22.05";
+    stateVersion = "22.05";
 
-      username = customization.username;
-    };
+    username = customization.username;
+  };
 
-    nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-    programs = {
-      bash = {
-        enable = true;
-        historyControl = ["ignoredups" "ignorespace"];
-        historyFileSize = 200000;
-        historySize = 100000;
-        shellAliases = {
-          ls = "ls --color=auto";
-        };
-      };
-
-      git = {
-        enable = true;
-        extraConfig = {
-          core = {
-            editor = "code --wait";
-          };
-        };
-        userEmail = customization.email;
-        userName = customization.name;
-      };
-
-      home-manager.enable = true;
-
-      vscode = {
-        enable = true;
-        extensions = with pkgs.vscode-extensions; [
-          eamodio.gitlens
-          esbenp.prettier-vscode
-          kamadorueda.alejandra
-          streetsidesoftware.code-spell-checker
-          timonwong.shellcheck
-        ];
-        userSettings = {
-          "diffEditor.ignoreTrimWhitespace" = false;
-          "editor.formatOnSave" = true;
-          "editor.rulers" = [80];
-        };
+  programs = {
+    bash = {
+      enable = true;
+      historyControl = ["ignoredups" "ignorespace"];
+      historyFileSize = 200000;
+      historySize = 100000;
+      shellAliases = {
+        ls = "ls --color=auto";
       };
     };
 
-    xfconf.settings = {
-      xfce4-keyboard-shortcuts = {
-        "commands/custom/Print" = "flameshot gui";
+    git = {
+      enable = true;
+      extraConfig = {
+        core = {
+          editor = "code --wait";
+        };
       };
-      xfwm4 = {
-        "general/theme" = "Numix";
-        "general/workspace_count" = 2;
-      };
-      xsettings = {
-        "Net/ThemeName" = "Numix";
+      userEmail = customization.email;
+      userName = customization.name;
+    };
+
+    home-manager.enable = true;
+
+    vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        eamodio.gitlens
+        esbenp.prettier-vscode
+        kamadorueda.alejandra
+        streetsidesoftware.code-spell-checker
+        timonwong.shellcheck
+      ];
+      userSettings = {
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "editor.formatOnSave" = true;
+        "editor.rulers" = [80];
       };
     };
-  }
+  };
+
+  xfconf.settings = {
+    xfce4-keyboard-shortcuts = {
+      "commands/custom/Print" = "flameshot gui";
+    };
+    xfwm4 = {
+      "general/theme" = "Numix";
+      "general/workspace_count" = 2;
+    };
+    xsettings = {
+      "Net/ThemeName" = "Numix";
+    };
+  };
+}
