0e5df774c3b882452b65a0160824a8ff530b92db
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-11-27 13:24:06 +0100

    Migrate test environment from Docker to Nix for simplicity

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,14 +8,10 @@ main() {
   local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
   cd "$(dirname "${script_folder}")"
 
-  docker run --entrypoint sh --rm --volume "${PWD}":/workdir \
-    evolutics/travel-kit:0.8.0 -c \
-    'git ls-files -z | xargs -0 travel-kit check --'
+  git ls-files -z | xargs -0 nix run --no-write-lock-file \
+    github:evolutics/travel-kit -- check --
 
-  docker run --rm --volume "${PWD}":/workdir \
-    "$(DOCKER_BUILDKIT=1 docker build --build-arg ansible_lint=5.3.1 --quiet \
-      https://github.com/evolutics/code-cleaner-buffet.git#0.17.0)" \
-    ansible-lint
+  nix run nixpkgs#ansible-lint
 }
 
 main "$@"
