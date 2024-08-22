052378511cd1825dfafce399ce8aaac994b9606a
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-08-22 21:08:54 +0200

    List Git branches for overview, not just status

--- home.nix
+++ home.nix
@@ -94,7 +94,7 @@ in {
       historyControl = ["ignoredups" "ignorespace"];
       initExtra = builtins.readFile ./configuration/bash_init_extra.sh;
       shellAliases = {
-        a = "git status";
+        a = "git branch && git status";
         ll = "ls --all --classify -l";
         ls = "ls --color=auto";
       };
