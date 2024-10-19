76f625afdc67ab65cc48098f991e88110bca0658
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-10-19 16:48:37 +0200

    Switch to stable Nix for security

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   };
 
   outputs = {
--- scripts/apply.sh
+++ scripts/apply.sh
@@ -64,7 +64,7 @@ configure_home() {
   nix flake update
   # Retry due to https://github.com/nix-community/home-manager/issues/2033.
   # TODO: Remove workaround once issue is fixed.
-  retry_once nix run home-manager/master -- switch
+  retry_once nix run home-manager/release-24.05 -- switch
   unset NIX_CONFIG
 }
 
