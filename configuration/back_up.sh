ae71506cdb19201bf5f2f043072e9787b04f8d98
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-10-12 16:53:37 +0200

    Extract user notification

--- configuration/back_up.sh
+++ configuration/back_up.sh
@@ -2,16 +2,23 @@
 
 set -o errexit -o nounset -o pipefail
 
-cd -- "$(dirname -- "$0")"
-FREQUENCY="${PWD##*.}"
-readonly FREQUENCY
+notify_user() {
+  local -r message="$1"
+  su '{{ user }}' --command "notify-send '${message}'"
+}
 
-if /usr/bin/rsnapshot "${FREQUENCY}"; then
-  if (("${RANDOM}" % 7 == 0)); then
-    su '{{ user }}' --command "notify-send 'Backup ${FREQUENCY} succeeded'"
+main() {
+  cd -- "$(dirname -- "$0")"
+  local -r frequency="${PWD##*.}"
+
+  if /usr/bin/rsnapshot "${frequency}"; then
+    if (("${RANDOM}" % 7 == 0)); then
+      notify_user "Backup ${frequency} succeeded"
+    fi
+  else
+    notify_user "Backup ${frequency} failed, see: /var/log/rsnapshot.log"
+    exit 1
   fi
-else
-  su '{{ user }}' --command \
-    "notify-send 'Backup ${FREQUENCY} failed, see: /var/log/rsnapshot.log'"
-  exit 1
-fi
+}
+
+main "$@"
