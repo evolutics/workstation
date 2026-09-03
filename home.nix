8e40838f00331b814c6f418b8644d939833ef7d6
Benjamin Fischer <benjamin.fischer@evolutics.info>
2026-09-03 21:53:29 +0200

    Disable seemingly buggy Tiling Assistant
    It is called "Enhanced Tiling" in the settings UI under Ubuntu Desktop.
    
    Reason is this issue: Super+Right tiles a window to the right half, but
    then immediately undoes the move/resize; the cause may be the keyboard
    shortcut being processed twice.

--- home.nix
+++ home.nix
@@ -27,11 +27,8 @@ in {
       command = "gradia --screenshot";
       name = "Take editable screenshot";
     };
+    "org/gnome/shell" = {disabled-extensions = ["tiling-assistant@ubuntu.com"];};
     "org/gnome/shell/extensions/dash-to-dock" = {dock-fixed = false;};
-    "org/gnome/shell/extensions/tiling-assistant" = {
-      disable-tile-groups = true;
-      enable-tiling-popup = false;
-    };
   };
 
   fonts.fontconfig.enable = true;
